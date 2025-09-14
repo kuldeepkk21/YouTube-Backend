00cea7704b6148d931e047123fbdc959b5c494df
kuldeep <kellysonu21@gmail.com>
2025-09-14 08:09:40 +0530

    create login and logout user functionality with auth middleware

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
@@ -16,6 +17,10 @@ userRouter.route("/register").post(
         }
     ]),
     registerUser)
+
+userRouter.route("/login").post(loginUser)   
+
+userRouter.route("/logout").post(verifyJWT, logoutUser)  
 
 export {userRouter};
 
